0cc109710d09a1b70dbd88872d799f551c9e7061
khoa-omega <nvkhoa05@gmail.com>
2023-12-07 21:35:01 +0700

    [Add] Find all comment API

--- src/main/java/com/vti/blogapp/controller/CommentController.java
+++ src/main/java/com/vti/blogapp/controller/CommentController.java
@@ -4,6 +4,9 @@ import com.vti.blogapp.dto.CommentDto;
 import com.vti.blogapp.form.CommentCreateForm;
 import com.vti.blogapp.service.CommentService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @GetMapping("/api/v1/comments")
+    public Page<CommentDto> findAll(Pageable pageable) {
+        return commentService.findAll(pageable);
+    }
 
     @PostMapping("/api/v1/posts/{postId}/comments")
     public CommentDto create(
