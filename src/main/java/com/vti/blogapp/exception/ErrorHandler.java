d325dc3cdc04c67b6a998680ef56eecf5f718b4c
khoa-omega <nvkhoa05@gmail.com>
2023-12-14 20:17:06 +0700

    [Add] Custom annotation PostIdExists

--- src/main/java/com/vti/blogapp/exception/ErrorHandler.java
+++ src/main/java/com/vti/blogapp/exception/ErrorHandler.java
@@ -1,11 +1,13 @@
 package com.vti.blogapp.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -27,5 +29,19 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
         }
         var response = new ErrorResponse(message, errors);
         return new ResponseEntity<>(response, headers, status);
+    }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolation(
+            ConstraintViolationException exception) {
+        var message = "Sorry! Invalid form";
+        var errors = new HashMap<String, String>();
+        for (var constraint : exception.getConstraintViolations()) {
+            var key = constraint.getPropertyPath().toString();
+            var value = constraint.getMessage();
+            errors.put(key, value);
+        }
+        var response = new ErrorResponse(message, errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
